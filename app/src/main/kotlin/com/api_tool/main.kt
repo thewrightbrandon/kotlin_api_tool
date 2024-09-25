0bdc73cfd72c7a6737f55db0b69c3cb261860038
Brandon Wright <thewrightbrandon@gmail.com>
2024-09-25 19:54:32 -0400

    added main file structure

---
+++ app/src/main/kotlin/com/api_tool/main.kt
@@ -0,0 +1,11 @@
+package com.api_tool
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+fun main () {
+    println("Jello, World!")
+}
